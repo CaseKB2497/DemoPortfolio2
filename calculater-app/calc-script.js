b4fd4026f3493c27414dd8db563cc57c1b343bd3
Kristopher Case <kristopher.case@fdmgroup.com>
2023-02-04 11:44:17 -0600

    adding new calculator js app to eventually display in projects.html

---
+++ calculater-app/calc-script.js
@@ -0,0 +1,41 @@
+let runningTotal = 0;
+let buffer = "0";
+let previousOperator;
+
+
+const screen = document.querySelector('.screen');
+
+function buttonClick(value){
+    if(isNaN(value)){
+        handleSymbol(value);
+    } else {
+        handleNumber(value);
+    }
+    screen.innerText = buffer;
+}
+
+function handleSymbol(symbol){
+    switch(symbol){
+        case 'C':
+            buffer = '0';
+            runningTotal = 0;
+            break;
+        case '=':
+            if(previousOperator === null){
+                return;
+            }
+            flushOpertion(parseInt(buffer));
+            previousOperator = null;
+            buffer = runningTotal;
+            runningTotal = 0;
+            break;
+        case '←':
+            if(buffer.length === 1){
+                buffer = '0';
+            } else {
+                buffer = buffer.toString(0, buffer.length -1);
+            }
+            break;
+
+    }
+}
